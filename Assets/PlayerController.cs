e8a9a382915eabb49b7a6c0b7161e291237fbb12
Samuel Egiuino <samueleguino97@gmail.com>
2020-12-30 14:52:02 -0400

    init

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public NavMeshAgent agent;
+    public Camera mainCamera;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+     agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1)){
+                animator.SetBool("isWalking",true);
+            RaycastHit hit;
+            if(Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),out hit)){
+                agent.destination = hit.point;
+                transform.LookAt(hit.point);
+            }
+        }
+         if (!agent.pathPending)
+ {
+     if (CheckDestinationReached(agent.destination))
+     {
+        
+                animator.SetBool("isWalking",false);
+         
+     }
+ }
+    }
+    bool CheckDestinationReached(Vector3 target) {
+  float distanceToTarget = Vector3.Distance(transform.position, target);
+  return distanceToTarget < 1;
+  
+}
+}
